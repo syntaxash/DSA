e92a1a235238637d1feb4b3a72bf36da0d183201
syntaxash <sharmaashish29012005@gmail.com>
2026-09-18 01:49:11 +0530

    union of sorted array

---
+++ union_of_two sorted_array.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> union_array(vector<int> a, vector<int> b, int n1, int n2)
+{
+    int i = 0;
+    int j = 0;
+    vector<int> unionArr;
+    while (i < n1 && j < n2)
+    {
+        if (a[i] <= b[j])
+        {
+            if (unionArr.size() == 0 || unionArr.back() != a[i])
+            {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else
+        {
+            if(unionArr.size() == 0 || unionArr.back() != b[j])
+            {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+
+    while (i < n1)
+    {
+        if (unionArr.size() == 0 || unionArr.back() != a[i])
+        {
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+    while (j < n2)
+    {
+        if (unionArr.size() == 0 || unionArr.back() != b[j])
+        {
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+    return unionArr;
+}
+int main()
+{
+
+    int n1;
+    cin >> n1;
+    vector<int> a(n1);
+    for (int i = 0; i < n1; i++)
+    {
+        cin >> a[i];
+    }
+
+    int n2;
+    cin >> n2;
+    vector<int> b(n2);
+    for (int j = 0; j < n2; j++)
+    {
+        cin >> b[j];
+    }
+
+    vector<int> result = union_array(a, b , n1 , n2);
+
+    for (auto i : result)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
